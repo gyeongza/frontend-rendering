ab2df2a61cc8bf713c739cc892a912cc5357b21a
gyeongza <gyeongza@naver.com>
2023-10-08 22:59:45 +0900

    feat: read 기능 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import Link from 'next/link';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -6,14 +7,41 @@ export const metadata: Metadata = {
   description: 'code review plaform',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const resp = await fetch('http://localhost:9999/topics');
+  const topics = await resp.json();
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body>
+        <h1>
+          <Link href="/">WEB</Link>
+        </h1>
+        <ol>
+          {topics.map((topic: any) => {
+            return (
+              <li key={topic.id}>
+                <Link href={`/read/${topic.id}`}>{topic.title}</Link>
+              </li>
+            );
+          })}
+        </ol>
+        {children}
+        <ul>
+          <li>
+            <Link href="/create">Create</Link>
+          </li>
+          <li>
+            <Link href="/update">Update</Link>
+          </li>
+          <li>
+            <input type="button" value="delete" />
+          </li>
+        </ul>
+      </body>
     </html>
   );
 }
